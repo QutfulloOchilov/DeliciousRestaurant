86a1d5c1bcdd973b245944aeb56826c4dbb67055
qutfullo <qutfullo1994@gmail.com>
2019-11-16 18:50:45 +0500

    improving login form

--- Client/Mobile/src/pages/Login/components/LoginForm.tsx
+++ Client/Mobile/src/pages/Login/components/LoginForm.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import {Component} from 'react';
 import {LoginFormValues} from '../LoginPage';
 import {FormikActions, Field, Formik, FormikProps} from 'formik';
-import {Button, View, TextInput, Text} from 'react-native';
+import {View, Text} from 'react-native';
+import {Button, TextInput, Provider as PaperProvider} from 'react-native-paper';
 
 interface Props {}
 type AllProps = Props;
@@ -10,14 +11,18 @@ class LoginForm extends Component<AllProps> {
   render() {
     return (
       <View>
-        <View
-          style={{
-            flex: 1,
-          }}>
+        <View style={{flex: 1, flexDirection: 'column', padding: 20}}>
           <Text>text</Text>
           <TextInput style={{backgroundColor: '#ededed', height: 60}} />
           <Text>text</Text>
           <TextInput style={{backgroundColor: '#ededed', height: 60}} />
+          <Button
+            icon="camera"
+            mode="contained"
+            style={{height: 40, backgroundColor: '#1e90ff'}}
+            onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
         </View>
       </View>
     );
