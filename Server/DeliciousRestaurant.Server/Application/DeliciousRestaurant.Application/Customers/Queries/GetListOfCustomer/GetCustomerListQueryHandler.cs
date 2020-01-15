bddd39c86e57ae75d5a4c296abf93e18e1def856
qutfullo <qutfullo1994@gmail.com>
2020-01-15 16:05:17 +0500

    implement logic of query handlers

--- Server/DeliciousRestaurant.Server/Application/DeliciousRestaurant.Application/Customers/Queries/GetListOfCustomer/GetCustomerListQueryHandler.cs
+++ Server/DeliciousRestaurant.Server/Application/DeliciousRestaurant.Application/Customers/Queries/GetListOfCustomer/GetCustomerListQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using DeliciousRestaurant.Application.Customers.Data;
+﻿using AutoMapper;
+using DeliciousRestaurant.Application.Customers.Data;
+using DeliciousRestaurant.Application.Interfaces;
 using DeliciousRestaurant.Application.Queries;
 using System;
 using System.Collections.Generic;
@@ -9,9 +11,18 @@ namespace DeliciousRestaurant.Application.Customers.Queries.GetListOfCustomer
 {
     public class GetCustomerListQueryHandler : BaseQueryHandler<IGetCustomerListQuery, IEnumerable<CustomerDTO>>
     {
-        public override Task<IEnumerable<CustomerDTO>> Handle(IGetCustomerListQuery request, CancellationToken cancellationToken)
+        public GetCustomerListQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
-            throw new NotImplementedException();
+            UnitOfWork = unitOfWork;
+            Mapper = mapper;
+        }
+        public IUnitOfWork UnitOfWork { get; }
+        public IMapper Mapper { get; }
+
+        public override Task<IEnumerable<CustomerDTO>> Handle(IGetCustomerListQuery request, CancellationToken cancellationToken = default)
+        {
+            var customer = UnitOfWork.CustomerRepository.GetByIdAsync(;
+            return MapAsync<Customer, CustomerDTO>(Mapper, customer);
         }
     }
 }
